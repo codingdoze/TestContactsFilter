d0182418891768e69c2d5e6978de20c0f041b16a
akshays <akshaydilkap10@gmail.com>
2017-07-20 15:28:02 +0530

    commit

--- app/src/main/java/com/example/akshays/testcontactsfilter/CustomRecylerAdapter.java
+++ app/src/main/java/com/example/akshays/testcontactsfilter/CustomRecylerAdapter.java
@@ -35,33 +35,6 @@ public class CustomRecylerAdapter extends RecyclerView.Adapter<CustomRecylerAdap
     public void onBindViewHolder(MyViewHolder holder, int position) {
 ContactsList list = contactsList.get(position);
         holder.names.setText(list.getNames());
-
-    }
-
-    public static CharSequence highlight(String search, String originalText) {
-        // ignore case and accents
-        // the same thing should have been done for the search text
-        String normalizedText = Normalizer.normalize(originalText, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "").toLowerCase();
-
-        int start = normalizedText.indexOf(search);
-        if (start < 0) {
-            // not found, nothing to to
-            return originalText;
-        } else {
-            // highlight each appearance in the original text
-            // while searching in normalized text
-            Spannable highlighted = new SpannableString(originalText);
-            while (start >= 0) {
-                int spanStart = Math.min(start, originalText.length());
-                int spanEnd = Math.min(start + search.length(), originalText.length());
-
-                highlighted.setSpan(new BackgroundColorSpan(Color.YELLOW), spanStart, spanEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-                start = normalizedText.indexOf(search, spanEnd);
-            }
-
-            return highlighted;
-        }
     }
     @Override
     public int getItemCount() {
